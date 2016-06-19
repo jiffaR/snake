7943f165e90c57432d9e46be157deff4ed6246a0
Denis <freinir@gmail.com>
2016-06-19 18:35:49 +0500

    Создание класса Фигура. Наследование вертикальной и горизонтальной линий от фигуры

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-           
-            p1.Draw();
+            Console.Clear();
+            Console.SetWindowSize(100, 50);
+            Console.SetBufferSize(120, 30);
 
-            Point p2 = new Point(4, 5, '#');
-
-            p2.Draw();
-
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Draw();
-            VerticalLine vLine = new VerticalLine(6, 10, 7, '@');
-            vLine.Draw();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
             Console.ReadLine();
         }
